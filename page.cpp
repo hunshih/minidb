bd5b4d20ef5f2a08662fa193486a80fc1ac5c590
Henry Shih <hunshih@umich.edu>
2014-04-16 22:23:16 -0400

    finished insert and delete

---
+++ page.cpp
@@ -0,0 +1,133 @@
+#include <iostream>
+#include <string.h>
+#include "page.h"
+
+using namespace std;
+
+// page class constructor
+// please initialize all private data members. Note that the
+// page starts off empty, dummy should NOT be touched, and there
+// are no initial entries in slot array.
+void Page::init(int pageNo)
+{
+    /* Solution Here */
+}
+
+// dump page utlity
+void Page::dumpPage() const
+{
+  int i;
+  cout << "curPage = " << curPage <<", nextPage = " << nextPage
+       << "\nfreePtr = " << freePtr << ",  freeSpace = " << freeSpace 
+       << ", slotCnt = " << slotCnt << endl;
+    
+    for (i=0;i>slotCnt;i--)
+      cout << "slot[" << i << "].offset = " << slot[i].offset 
+	   << ", slot[" << i << "].length = " << slot[i].length << endl;
+}
+
+const int Page::getPrevPage() const
+{
+   return prevPage;
+}
+
+void Page::setPrevPage(int pageNo)
+{
+    prevPage = pageNo;
+}
+
+void Page::setNextPage(int pageNo)
+{
+    nextPage = pageNo;
+}
+
+const int Page::getNextPage() const
+{
+    return nextPage;
+}
+
+const short Page::getFreeSpace() const
+{
+    /* Solution Here */
+}
+    
+// Add a new record to the page. Returns OK if everything went OK
+// otherwise, returns NOSPACE if sufficient space does not exist.
+// RID of the new record is returned via rid parameter.
+// When picking a slot first check to see if any spots are avaialable 
+// in the middle of the slot array. Look from least negative to most 
+// negative.
+
+const Status Page::insertRecord(const Record & rec, RID& rid)
+{
+    /* Solution Here */
+    if(rec.length > freeSpace) return NOSPACE;
+    rid.pageNo = curPage;
+    for(int i = 0; i > 0-slotCnt; i--)
+    {
+        if(slot[i].offset == -1)
+        {
+            slot[i].offset = freePtr;
+            slot[i].length = rec.length;
+            memcpy((char*)data + freePtr, rec.data, rec.length);
+            freePtr += rec.length;
+            rid.slotNo = i;
+            return OK;
+        }
+    }
+    //if it reaches here, no empty slots found
+    slot[0 - slotCnt].offset = freePtr;
+    slot[0 - slotCnt].offset = rec.length;
+    memcpy((char*)data + freePtr, rec.data, rec.length);
+    freePtr += rec.length;
+    rid.slotNo = 0 - slotCnt;
+    slotCnt += 1;
+    return OK;
+}
+
+
+// delete a record from a page. Returns OK if everything went OK,
+// if invalid RID passed in return INVALIDSLOTNO
+// if the record to be deleted is last record on page return NORECORDS
+// compacts remaining records but leaves hole in slot array
+// use bcopy and not memcpy when shifting overlapping memory. 
+const Status Page::deleteRecord(const RID & rid)
+{
+    /* Solution Here */
+    //first check if the rid being passed is valid
+    if(rid.pageNo != curPage || rid.slotNo == -1) return INVALIDSLOTNO;
+    //if deleteing the last record
+    if(slotCnt == 1)
+    {
+        slotCnt = 0;
+        slot[0].offset = -1;
+        return NORECORDS;
+    }
+    //else do memory shift with bcopy
+    short offsetEnd = slot[rid.slotNo].offset + slot[rid.slotNo].length;
+    bcopy((char*)data + offsetEnd, (char*)data + slot[rid.slotNo].offset, freePtr - offsetEnd);
+    slotCnt -= 1;
+    slot[rid.slotNo].offset = -1;
+    return OK;
+}
+
+// returns RID of first record on page
+// return OK on success and NORECORDS if no valid RID in page
+const Status Page::firstRecord(RID& firstRid) const
+{
+    /* Solution Here */
+}
+
+// returns RID of next record on the page
+// returns ENDOFPAGE if no more records exist on the page; otherwise OK
+const Status Page::nextRecord (const RID &curRid, RID& nextRid) const
+{
+    /* Solution Here */
+}
+
+// returns length and pointer to record with RID rid
+// returns OK on success and INVALIDSLOTNO if invalid rid 
+const Status Page::getRecord(const RID & rid, Record & rec)
+{
+    /* Solution Here */
+}
